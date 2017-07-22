4ce7e291762e59c936a43fb004446c727f659b3c
Daniel Dao <daniel.vu.dao@civitaslearning.com>
2017-07-22 15:46:57 -0700

    chapter 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,11 +49,20 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a min length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
