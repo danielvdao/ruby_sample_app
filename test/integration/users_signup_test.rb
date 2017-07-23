596b87715d9e96b50a9bf6356a14facdc1dc7b2a
Daniel Dao <daniel.vu.dao@civitaslearning.com>
2017-07-23 16:26:28 -0700

    before DB reset

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,14 +6,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # end
   test "invalid signup path" do
     get signup_path
+    assert_select "form[action=?]", signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
+      post signup_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password: "foo",
                                          password_confirmation: "bar" } }
     end
 
-    assert_template 'users/new'
+    assert_template "users/new"
+    assert_select "div#error_explanation" # test that the error explanation div class is there
+    assert_select "div.alert.alert-danger" # test that alert is there
   end
 
 end
